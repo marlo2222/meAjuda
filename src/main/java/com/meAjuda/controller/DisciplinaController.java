c7c0e555321b209ec0f5b25e4942070cb4ce7c0a
marlo2222 <marlo_henrique@hotmail.com.br>
2019-10-19 12:09:50 -0300

    adicionando consultas

--- src/main/java/com/meAjuda/controller/DisciplinaController.java
+++ src/main/java/com/meAjuda/controller/DisciplinaController.java
@@ -1,25 +1,55 @@
 package com.meAjuda.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.meAjuda.pojo.Curso;
+import com.meAjuda.pojo.Disciplina;
 import com.meAjuda.pojo.Usuario;
+import com.meAjuda.services.DisciplinaService;
 
 @Controller
 public class DisciplinaController {
 	
+	@Autowired
+	DisciplinaService disciplinaService;
+	
 	@GetMapping("/cadastro/disciplina")
 	public ModelAndView cadastraDisciplina() {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("cadastro/cadastroDisciplina");
+		List<?> cursos = disciplinaService.listarCursos();
+		mv.addObject("disciplina", new Disciplina());
+		mv.addObject("cursos", cursos);
+		//mv.setViewName("cadastro/cadastroDisciplina");
+		mv.setViewName("testes/curso");
 		return mv;
 	}
 	
 	@GetMapping("/cadastro/curso")
 	public ModelAndView cadastraCurso() {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("cadastro/cadastroCurso");
+		mv.addObject("curso", new Curso());
+		//mv.setViewName("cadastro/cadastroCurso");
+		mv.setViewName("testes/curso");
+		return mv;
+	}
+	@PostMapping("/cadastro/curso")
+	public ModelAndView cadastraCurso(Curso curso, BindingResult result) {
+		ModelAndView mv = new ModelAndView();
+		disciplinaService.imprimir(curso);
+		disciplinaService.salvarCurso(curso,result);
+		if (result.hasErrors()) {
+			mv.setViewName("testes/curso"); 	
+		}else {
+		mv.setViewName("redirect:/login");
+		}
+		//mv.setViewName("cadastro/cadastroCurso");
 		return mv;
 	}
 	
--- src/main/java/com/meAjuda/services/DisciplinaService.java
+++ src/main/java/com/meAjuda/services/DisciplinaService.java
@@ -2,7 +2,10 @@ package com.meAjuda.services;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.client.RestTemplate;
 
 import com.meAjuda.pojo.Curso;
@@ -10,13 +13,29 @@ import com.meAjuda.pojo.Curso;
 @Service
 public class DisciplinaService {
 	
-	private final String url = "https://passei-disciplina.herokuapp.com/api/curso/listar";
+	private final String urlCurso = "https://passei-disciplina.herokuapp.com/api/curso/";
 	
 	RestTemplate rest = new RestTemplate();
 	
 	public List<?> listarCursos(){
-		List<Curso> cursos = rest.getForObject(url, List.class);
+		List<Curso> cursos = rest.getForObject(urlCurso+"listar", List.class);
 		return cursos;
+	}
+	public BindingResult salvarCurso(Curso curso, BindingResult bindingResult){
+		
+		Curso newCurso = rest.postForObject(urlCurso+"salvar", curso, Curso.class);
+		
+		if (newCurso == null) {
+			bindingResult.rejectValue("curso", "", "não foi posivel salvar o curso");
+			return bindingResult;
+		}
+		return bindingResult;
+		
+	}
+	
+	public void imprimir(Curso curso){
+		System.out.println(curso.getNome());
+		System.out.println(curso.getQtdSesmestres());
 	}
 	
 	
